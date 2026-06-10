1ce99e0006db24c69cef746f117e81d672534031
viswanathan11 <m.vishwanthan06@gmail.com>
2026-06-10 11:28:22 +0530

    Refactor SumOfAdjacentCell to improve output clarity and structure

--- Graph Theory/SumOfAdjacentCell.java
+++ Graph Theory/SumOfAdjacentCell.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class SumOfAdjacentCell{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the i and j of the cell to calculate its adjacent cell sum");
         int r=sc.nextInt();
         int c=sc.nextInt();
         int[][] arr={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
@@ -14,6 +15,16 @@ public class SumOfAdjacentCell{
         {13,14,15,16}
         */
         int[][] diff={{-1,0},{1,0},{0,-1},{0,1},{-1,-1},{-1,1},{1,-1},{1,1}};
-        
+
+        int sum=0;
+        for(int i=0;i<diff.length;i++){
+            int row=r+diff[i][0];
+            int col=c+diff[i][1];
+            if(row>=0 && col>=0 && row<arr.length && col<arr[0].length)
+            sum+=arr[row][col];
+        }
+
+        System.out.println("The sum of Adjacent cell of ("+r+","+c+") is "+sum);
+        sc.close();
     }
 }
