de9f06188683bca8f790f67b6b71c4d8b793ca1a
viswanathan11 <m.vishwanthan06@gmail.com>
2026-07-25 10:37:03 +0530

    Remove unnecessary blank line before main method in DisjointSet class

--- DisjointSet/DisjointSet.java
+++ DisjointSet/DisjointSet.java
@@ -18,7 +18,6 @@ public class DisjointSet {
             ldr[leader2]=leader1;
         }
     }
-
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
