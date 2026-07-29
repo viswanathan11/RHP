5600dc037e212d770a35da6dfb7cfc15555e7445
viswanathan11 <m.vishwanthan06@gmail.com>
2026-07-29 09:30:24 +0530

    Add StaticRangeMinimumQueries class with initial structure for range minimum queries

---
+++ CSES/StaticRangeMinimumQueries.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class StaticRangeMinimumQueries {
+    static int find(int[] seg,int node,int nl,int nr,int ql,int qr){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+    //BRUTE FORCE
+    // public static void main(String[] args) {
+    //     Scanner sc=new Scanner(System.in);
+    //     int n=sc.nextInt();
+    //     int q=sc.nextInt();
+
+    //     int[] arr=new int[n+1];
+
+    //     for(int i=1;i<=n;i++){
+    //         arr[i]=sc.nextInt();
+    //     }
+
+    //     while(q-->0){
+    //         int min=Integer.MAX_VALUE;
+    //         int st=sc.nextInt();
+    //         int end=sc.nextInt();
+
+    //         for(int i=st;i<=end;i++){
+    //             min=Integer.min(min,arr[i]);
+    //         }
+
+    //         System.out.println(min);
+    //     }
+    // }
+}
